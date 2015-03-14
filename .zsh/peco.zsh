edec7e98f9f7bb5653848d23c0bbdcd13026537a
kg8m <info@kagitaku.com>
2015-03-14 19:33:47 +0900

    use mollifier/anyframe for peco

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -1,29 +1,8 @@
 builtin command -v peco > /dev/null || return
 
-# http://k0kubun.hatenablog.com/entry/2014/07/06/033336
-# http://blog.livedoor.jp/abell2142/archives/53185576.html
-function peco-select-history() {
-  typeset tac
+fpath=(~/.peco/anyframe(N-/) $fpath)
 
-  if builtin command -v tac > /dev/null; then
-    tac=tac
-  elif builtin command -v gtac > /dev/null; then
-    tac=gtac
-  else
-    tac='tail -r'
-  fi
+autoload -Uz anyframe-init
+anyframe-init
 
-  BUFFER=$(history -n 1 | eval $tac | peco --query "$LBUFFER")
-  CURSOR=$#BUFFER
-  zle -R -c
-}
-zle -N peco-select-history
-bindkey '^r' peco-select-history
-
-# http://k0kubun.hatenablog.com/entry/2014/07/06/033336
-function peco-pkill() {
-  for pid in `ps aux | peco | awk '{ print $2 }'`; do
-    kill $pid
-    echo "Killed ${pid}"
-  done
-}
+bindkey '^r' anyframe-widget-put-history
