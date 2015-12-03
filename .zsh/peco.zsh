abea31d103e5b77b44a393c73cc1755b59ff8ede
kg8m <info@kagitaku.com>
2015-12-03 12:21:56 +0900

    remove unnecessary settings for peco: "Layout" in configuration file works on version 0.3.5  Please enter the commit message for your changes. Lines starting

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -6,7 +6,3 @@ autoload -Uz anyframe-init
 anyframe-init
 
 bindkey '^r' anyframe-widget-put-history
-
-# "Layout" setting in configuration file not working
-zstyle ':anyframe:selector:peco:' command 'peco --layout bottom-up'
-alias peco='peco --layout bottom-up'
