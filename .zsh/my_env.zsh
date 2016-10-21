cc6ed820229efa37f468e34856f118aa3e543022
kg8m <takumi.kagiyama@gmail.com>
2016-10-21 19:29:16 +0900

    also check existence of Gemfile

--- .zsh/my_env.zsh
+++ .zsh/my_env.zsh
@@ -1,7 +1,7 @@
 export LOCAL_GEMFILE=Gemfile.local
 
 set_bundle_gemfile() {
-  if [[ -f $LOCAL_GEMFILE ]]; then
+  if [[ -f Gemfile && -f $LOCAL_GEMFILE ]]; then
     [[ ! $BUNDLE_GEMFILE ]] && export BUNDLE_GEMFILE=$LOCAL_GEMFILE
   else
     [[ $BUNDLE_GEMFILE = $LOCAL_GEMFILE ]] && export BUNDLE_GEMFILE=
