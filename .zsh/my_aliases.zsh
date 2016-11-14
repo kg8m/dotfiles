0bce152e0840a73e88707b66bd9c2e86018a6c1a
kg8m <takumi.kagiyama@gmail.com>
2016-11-14 20:16:49 +0900

    remove unused aliases and adjust whitespaces

--- .zsh/my_aliases.zsh
+++ .zsh/my_aliases.zsh
@@ -4,7 +4,6 @@ alias -g L="| less"
 alias -g H="| head"
 alias -g T="| tail"
 alias -g P="| peco"
-alias -g DIFF_MINIMAL="--diff-cmd /usr/bin/diff -x '-b -B -U 10'"
 alias -g FFTEST="TESTOPTS='--runner=failfast'"
 
 alias g='git'
@@ -16,7 +15,7 @@ else
 fi
 
 if [ -f /Applications/MacVim.app/Contents/MacOS/Vim ]; then
-  macvim_cmd='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+  macvim_cmd='    env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
   macvimdiff_cmd='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff "$@"'
   alias vi=${macvim_cmd}
   alias vim=${macvim_cmd}
@@ -32,23 +31,15 @@ fi
 alias _ls='/bin/ls -a'
 alias ls='ls --color -a'
 alias ll='ls -l'
-alias less='less -R'
-alias vless='/usr/local/share/vim/vim73/macros/less.sh'
-alias rak='rak --sort-files'
-alias rak_app="rak -k 'db/|log/|public/|spec/|test/|tmp/|vendor/'"
-alias ag='ag --hidden --pager "less -R" -S'
-alias pt='pt --color -HSe --parallel'
 alias diff='diff -U 10 -b -B'
 alias _diff='/usr/bin/diff'
 
-alias 80='sudo ruby script/server webrick -p 80'
-alias 443='sudo ruby script/webrick_ssl -p 443'
-alias log='tail -F log/development.log log/production*.log'
+alias log='     tail -F log/development.log log/production*.log'
 alias log_test='tail -F log/test.log'
 
 alias console='ruby script/console'
-alias about='ruby script/about'
-alias prepare='execute_with_echo "rake db:test:prepare"'
+alias prepare='       execute_with_echo "rake db:test:prepare"'
+alias load_structure='execute_with_echo "rake db:test:load_structure"'
 alias rake_units='      execute_with_echo "TEST_ENV_NUMBER=2 prepare; TEST_ENV_NUMBER=2 rake test:units $1"'
 alias rake_functionals='execute_with_echo "TEST_ENV_NUMBER=3 prepare; TEST_ENV_NUMBER=3 rake test:functionals $1"'
 alias rake_integration='execute_with_echo "TEST_ENV_NUMBER=4 prepare; TEST_ENV_NUMBER=4 rake test:integration $1"'
@@ -60,7 +51,6 @@ alias frake_integration='execute_with_echo "rake_integration FFTEST"'
 alias rake_models='     execute_with_echo "TEST_ENV_NUMBER=2 rake spec:models"'
 alias rake_controllers='execute_with_echo "TEST_ENV_NUMBER=3 rake spec:controllers"'
 alias rake_helpers='    execute_with_echo "TEST_ENV_NUMBER=4 rake spec:helpers"'
-alias ruby_multitest='ruby -I ${RUBYGEMS_PATH}rake-*/lib ${RUBYGEMS_PATH}rake-*/lib/rake/rake_test_loader.rb --runner=failfast'
 
 alias reload='source ~/.zshrc; echo "~/.zshrc sourced."'
 
