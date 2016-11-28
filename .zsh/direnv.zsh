9bfb066d6c8db1c4b2b3f191183f521f32f99edc
kg8m <takumi.kagiyama@gmail.com>
2016-11-29 08:21:31 +0900

    warn if .envrc exists but direnv is not installed

--- .zsh/direnv.zsh
+++ .zsh/direnv.zsh
@@ -1,3 +1,10 @@
 if which direnv > /dev/null 2>&1; then
   eval "$( direnv hook zsh )"
+else
+  error_for_envrc() {
+    if [[ -f .envrc ]]; then
+      >&2 echo "WARNING: .envrc exists but direnv isn't installed."
+    fi
+  }
+  add-zsh-hook chpwd error_for_envrc
 fi
