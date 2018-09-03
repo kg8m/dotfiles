55edd669b389ee13c3d82689f6f3b7cbf90eff87
kg8m <takumi.kagiyama@gmail.com>
2018-09-03 17:26:32 +0900

    Use double quotations

--- .zsh/my_aliases.zsh
+++ .zsh/my_aliases.zsh
@@ -4,15 +4,15 @@ alias -g L="| less"
 alias -g H="| head"
 alias -g T="| tail"
 alias -g F="| \$FILTER"
-alias -g GW='--ignore-all-space --ignore-blank-lines --ignore-space-at-eol --ignore-space-change'  # for Git: ignore whitespace changes
+alias -g GW="--ignore-all-space --ignore-blank-lines --ignore-space-at-eol --ignore-space-change"  # for Git: ignore whitespace changes
 
 alias ag="ag --pager=less --hidden --color-line-number='1;36' --color-match='30;46' --color-path='1;34'"
-alias g='git'
+alias g="git"
 
 if builtin command -v colorsvn > /dev/null; then
-  alias s='colorsvn'
+  alias s="colorsvn"
 else
-  alias s='svn'
+  alias s="svn"
 fi
 
 if [ -f /Applications/MacVim.app/Contents/MacOS/Vim ]; then
@@ -23,20 +23,20 @@ if [ -f /Applications/MacVim.app/Contents/MacOS/Vim ]; then
   alias vimdiff=${macvimdiff_cmd}
 fi
 
-alias rm='rm -i'
+alias rm="rm -i"
 
 if `crontab -i > /dev/null 2>&1`; then
-  alias crontab='crontab -i'
+  alias crontab="crontab -i"
 fi
 
-alias _ls='/bin/ls -a'
-alias ls='ls --color -a'
-alias ll='ls -l --time-style=long-iso'
-alias diff='diff -U 10 -b -B'
-alias _diff='/usr/bin/diff'
-alias _watch='/usr/bin/watch'
-alias watch='watch --color'
+alias _ls="/bin/ls -a"
+alias ls="ls --color -a"
+alias ll="ls -l --time-style=long-iso"
+alias diff="diff -U 10 -b -B"
+alias _diff="/usr/bin/diff"
+alias _watch="/usr/bin/watch"
+alias watch="watch --color"
 
-alias reload='source ~/.zshrc; echo "~/.zshrc sourced."'
+alias reload="source ~/.zshrc; echo "~/.zshrc sourced.""
 
 alias mux='attach_or_new_tmux'  # never use tmuxinator
