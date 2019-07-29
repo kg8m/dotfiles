cde3d4e8900a2e2850e686c3bc9431e19f220ddf
kg8m <takumi.kagiyama@gmail.com>
2019-07-29 20:56:04 +0900

    Check if goenv is available before configurations

--- .zsh/goenv.zsh
+++ .zsh/goenv.zsh
@@ -1,7 +1,9 @@
-export GOENV_ROOT=$HOME/.goenv
-export PATH=$GOENV_ROOT/bin:$PATH
+if [ -d ~/.goenv ]; then
+  export GOENV_ROOT=$HOME/.goenv
+  export PATH=$GOENV_ROOT/bin:$PATH
 
-eval "$( goenv init - )"
+  eval "$( goenv init - )"
 
-export PATH="$GOROOT/bin:$PATH"
-export PATH="$GOPATH/bin:$PATH"
+  export PATH="$GOROOT/bin:$PATH"
+  export PATH="$GOPATH/bin:$PATH"
+fi
