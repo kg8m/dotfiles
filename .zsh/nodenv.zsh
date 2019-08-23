0f14825ed6a2f4a27fba82601a2e603bcca79fc7
kg8m <takumi.kagiyama@gmail.com>
2019-08-23 21:37:36 +0900

    Use linebreaks

--- .zsh/nodenv.zsh
+++ .zsh/nodenv.zsh
@@ -1,4 +1,7 @@
 if [ -d ~/.nodenv ]; then
   export PATH=~/.nodenv/bin:$PATH
-  if which nodenv > /dev/null 2>&1; then eval "$( nodenv init - )"; fi
+
+  if which nodenv > /dev/null 2>&1; then
+    eval "$( nodenv init - )"
+  fi
 fi
