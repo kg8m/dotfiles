da54eecd34afc2bb7d34bef232ef44f12cd4f213
kg8m <takumi.kagiyama@gmail.com>
2019-10-14 12:25:12 +0900

    Disable waiting zsh-autosuggestions to enable it at the start

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -3,5 +3,5 @@ zplugin ice silent wait; zplugin light zsh-users/zsh-completions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#666666,bold,underline"
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-zplugin ice silent wait; zplugin light zsh-users/zsh-autosuggestions
+zplugin ice silent; zplugin light zsh-users/zsh-autosuggestions
 bindkey '^' autosuggest-execute
