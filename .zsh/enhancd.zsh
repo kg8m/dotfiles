10c0ba01271da624988fa2faa4ce3585f9a04acd
kg8m <takumi.kagiyama@gmail.com>
2019-10-14 10:49:58 +0900

    Remove unnecessary wrapper function

--- .zsh/enhancd.zsh
+++ .zsh/enhancd.zsh
@@ -1,14 +1,11 @@
-function overwrite_cd {
-  function cd_with_mkdir() {
-    if [[ ! "$@" =~ "^$|^-$" ]] && [ ! -d "$@" ]; then
-      echo "$@ not exists"
-      execute_with_confirm "mkdir -p \"$@\""
-    fi
+function cd_with_mkdir() {
+  if [[ ! "$@" =~ "^$|^-$" ]] && [ ! -d "$@" ]; then
+    echo "$@ not exists"
+    execute_with_confirm "mkdir -p \"$@\""
+  fi
 
-    __enhancd::cd "$@"
-  }
-  alias cd="cd_with_mkdir"
+  __enhancd::cd "$@"
 }
 
 export ENHANCD_FILTER=filter
-zplugin ice silent wait atload"overwrite_cd"; zplugin light b4b4r07/enhancd
+zplugin ice silent wait atload'alias cd="cd_with_mkdir"'; zplugin light b4b4r07/enhancd
