1ea7483a81d6cb4902a962328a78da396a648741
kg8m <takumi.kagiyama@gmail.com>
2019-10-14 14:14:58 +0900

    Remove meaningless configs and add comments

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -3,4 +3,6 @@ zplugin ice silent wait; zplugin light zsh-users/zsh-completions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#666666,bold,underline"
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-zplugin ice silent; zplugin light zsh-users/zsh-autosuggestions
+
+# Don't use `zplugin ice wait` because it prevents autosuggestions startup
+zplugin light zsh-users/zsh-autosuggestions
