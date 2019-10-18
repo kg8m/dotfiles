1746f2e5018121be4b85292ec43faa70f8ef00da
kg8m <takumi.kagiyama@gmail.com>
2019-10-18 21:35:07 +0900

    Fix zsh completion: don't use `_expand` and `_prefix`

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,7 +1,7 @@
 # http://voidy21.hatenablog.jp/entry/20090902/1251918174
 # https://qiita.com/kotashiratsuka/items/ae37757aa1d31d1d4f4d
 zstyle ':completion:*' menu select=1
-zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*' completer _complete _match _approximate _list _history
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' keep-prefix
 zstyle ':completion:*' use-cache true
