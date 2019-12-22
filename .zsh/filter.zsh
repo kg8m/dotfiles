f919fe42cd69cf20fb02a8b6b2c1f1d6cc75c347
kg8m <takumi.kagiyama@gmail.com>
2019-12-22 18:47:30 +0900

    Use fzf if available

--- .zsh/filter.zsh
+++ .zsh/filter.zsh
@@ -2,8 +2,23 @@ function setup_anyframe {
   autoload -Uz anyframe-init
   anyframe-init
 
-  zstyle ":anyframe:selector:" use peco
-  zstyle ":anyframe:selector:peco:" command filter
+  if which fzf > /dev/null 2>&1; then
+    local fzf_binds=""
+    fzf_binds+="ctrl-f:page-down"
+    fzf_binds+=",ctrl-b:page-up"
+    fzf_binds+=",ctrl-space:toggle+down"
+    fzf_binds+=",ctrl-h:backward-char"
+    fzf_binds+=",ctrl-l:forward-char"
+    export FZF_DEFAULT_OPTS="--ansi --multi --bind=$fzf_binds"
+
+    zstyle ":anyframe:selector:" use fzf
+    zstyle ":anyframe:selector:fzf:" command filter
+  elif which peco > /dev/null 2>&1; then
+    zstyle ":anyframe:selector:" use peco
+    zstyle ":anyframe:selector:peco:" command filter
+  else
+    echo "Any filter is not found." >&2; return 1
+  fi
 
   bindkey "^R" anyframe-widget-put-history
 }
