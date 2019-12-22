4c4e7099817066b026406146e7478c26cf42b43e
kg8m <takumi.kagiyama@gmail.com>
2019-12-22 20:48:03 +0900

    Update fzf configs

--- .zsh/filter.zsh
+++ .zsh/filter.zsh
@@ -6,10 +6,10 @@ function setup_anyframe {
     local fzf_binds=""
     fzf_binds+="ctrl-f:page-down"
     fzf_binds+=",ctrl-b:page-up"
-    fzf_binds+=",ctrl-space:toggle+down"
+    fzf_binds+=",ctrl-space:toggle+up"
     fzf_binds+=",ctrl-h:backward-char"
     fzf_binds+=",ctrl-l:forward-char"
-    export FZF_DEFAULT_OPTS="--ansi --multi --bind=$fzf_binds"
+    export FZF_DEFAULT_OPTS="--ansi --multi --select-1 --bind=$fzf_binds"
 
     zstyle ":anyframe:selector:" use fzf
     zstyle ":anyframe:selector:fzf:" command filter
