cc41d92650fb05b7d8985665b565b60a41496615
kg8m <takumi.kagiyama@gmail.com>
2019-12-30 00:48:37 +0900

    Update fzf configs

--- .zsh/filter.zsh
+++ .zsh/filter.zsh
@@ -9,16 +9,20 @@ function setup_anyframe {
     binds+=",tab:toggle-preview"
 
     local options=""
-    options+="--ansi"
+    options+=" --ansi"
     options+=" --bind='$binds'"
     options+=" --exit-0"
+    options+=" --info=inline"
     options+=" --multi"
+    options+=" --no-sort"
+    options+=" --no-unicode"
     options+=" --reverse"
-    options+=" --preview='echo {}'"
+    options+=" --preview='echo {} | head -n3'"
     options+=" --preview-window='down:3:hidden:wrap'"
     options+=" --select-1"
 
     export FZF_DEFAULT_OPTS="$options"
+    export FZF_DEFAULT_COMMAND="source ~/.zsh/my_functions.zsh; my_grep_without_pager --files"
   }
 
   autoload -Uz anyframe-init
