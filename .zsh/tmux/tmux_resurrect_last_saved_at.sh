4c9fbc7b4ebdcd639cbbb0688eeac17c72b2a0a4
kg8m <takumi.kagiyama@gmail.com>
2020-01-01 23:43:18 +0900

    Add linebreaks

--- .zsh/tmux/tmux_resurrect_last_saved_at.sh
+++ .zsh/tmux/tmux_resurrect_last_saved_at.sh
@@ -1,1 +1,5 @@
-ls -l ~/.tmux/resurrect/last | awk '{print $NF}' | egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}' -o | sed -e 's/T/ /' | sed -e 's/-/\//g'
+ls -l ~/.tmux/resurrect/last |
+  awk '{print $NF}' |
+  egrep '[0-9]{4}-?[0-9]{2}-?[0-9]{2}T[0-9]{2}:?[0-9]{2}' -o |
+  sed -e 's/T/ /' |
+  sed -e 's/-/\//g'
