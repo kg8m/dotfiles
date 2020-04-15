b503887e8f84ed5bdf9c271f70ce6fffc56a3da8
kg8m <takumi.kagiyama@gmail.com>
2020-04-15 21:07:13 +0900

    Show each branch's info when selecting branches

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -1,2 +1,2 @@
-export GIT_PROTECTED_LOCAL_BRANCH_PATTERN='^[ *]+master$'
-export GIT_PROTECTED_REMOTE_BRANCH_PATTERN='\borigin/master$'
+export GIT_PROTECTED_LOCAL_BRANCH_PATTERN='^[ *]+master +'
+export GIT_PROTECTED_REMOTE_BRANCH_PATTERN='^[ *]+origin/(HEAD|master) +'
