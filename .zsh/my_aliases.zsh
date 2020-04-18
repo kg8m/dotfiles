4e65b71aae7883bea9c343e3a5b4ee16e8b602a3
kg8m <takumi.kagiyama@gmail.com>
2020-04-18 16:40:16 +0900

    Remove disused zsh aliases

--- .zsh/my_aliases.zsh
+++ .zsh/my_aliases.zsh
@@ -5,42 +5,15 @@ alias -g H="| head"
 alias -g T="| tail"
 alias -g F="| filter"
 
-GIT_IGNORE_WHITESPACE_CHANGES=""
-GIT_IGNORE_WHITESPACE_CHANGES+=" --ignore-all-space"
-GIT_IGNORE_WHITESPACE_CHANGES+=" --ignore-blank-lines"
-GIT_IGNORE_WHITESPACE_CHANGES+=" --ignore-space-at-eol"
-GIT_IGNORE_WHITESPACE_CHANGES+=" --ignore-space-change"
-alias -g GW=$GIT_IGNORE_WHITESPACE_CHANGES
-unset GIT_IGNORE_WHITESPACE_CHANGES
-
-AG_OPTIONS=""
-AG_OPTIONS+=" --pager=less"
-AG_OPTIONS+=" --hidden"
-AG_OPTIONS+=" --workers=1"
-AG_OPTIONS+=" --color-line-number='1;36'"
-AG_OPTIONS+=" --color-match='30;46'"
-AG_OPTIONS+=" --color-path='1;34'"
-alias ag="ag $AG_OPTIONS"
-unset AG_OPTIONS
-
-alias g="git"
-
-function remove_with_confirm {
-  echo "Removing:"
-  execute_with_echo "ls $@"
-  execute_with_confirm "rm -fr $@"
-}
-alias rmc="remove_with_confirm"
-alias rm="rm -i"
-
-if `crontab -i > /dev/null 2>&1`; then
+if crontab -i > /dev/null 2>&1; then
   alias crontab="crontab -i"
 fi
 
-alias sudo='sudo env PATH=$PATH'
+alias diff="diff -U 10 -b -B"
 alias ls="ls --color -a"
 alias ll="ls -l --time-style=long-iso"
-alias diff="diff -U 10 -b -B"
+alias rm="rm -i"
+alias sudo='sudo env PATH=$PATH'
 alias watch="watch --color"
 
 alias reload="source ~/.zshrc; echo \"~/.zshrc sourced.\""
