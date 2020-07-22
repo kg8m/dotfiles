93e221d3e1b5a97f378ae879f07086f86a147d9a
kg8m <takumi.kagiyama@gmail.com>
2020-07-22 09:30:12 +0900

    Update configs for textlint

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   filters: {
     comments: true,
+    "node-types": {
+      nodeTypes: ["BlockQuote", "CodeBlock"],
+    },
   },
   rules: {
-    "textlint-rule-preset-ja-technical-writing": {
+    "@textlint-ja/no-synonyms": true,
+    "@textlint-ja/textlint-rule-no-insert-dropping-sa": true,
+    "date-weekday-mismatch": { lang: "ja-JP" },
+    "ja-hiragana-keishikimeishi": true,
+    "joyo-kanji": true,
+
+    // Disable because there are too many false positives
+    "no-hoso-kinshi-yogo": false,
+
+    "prefer-tari-tari": true,
+    "preset-ja-technical-writing": {
       "ja-no-weak-phrase": false,
       "ja-no-mixed-period": false,
       "no-doubled-joshi": {
@@ -18,14 +31,18 @@ module.exports = {
         ],
       },
     },
-    "textlint-rule-preset-jtf-style": {
+    "preset-jtf-style": {
       "3.1.1.全角文字と半角文字の間": false,
       "3.1.2.全角文字どうし": false,
       "3.3.かっこ類と隣接する文字の間のスペースの有無": false,
       "4.2.1.感嘆符(！)": false,
       "4.2.2.疑問符(？)": false,
       "4.2.7.コロン(：)": false,
+      "4.3.1.丸かっこ（）": false,
       "4.3.2.大かっこ［］": false,
+    },
+    "terminology": {
+      defaultTerms: true,
     },
   },
 };
