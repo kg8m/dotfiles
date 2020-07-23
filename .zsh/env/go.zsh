eba9ada80fda23f7da68f0f9630e90de7090eaf7
kg8m <takumi.kagiyama@gmail.com>
2020-07-23 16:14:27 +0900

    Fix detecting the newest Go directory path

--- .zsh/env/go.zsh
+++ .zsh/env/go.zsh
@@ -1,6 +1,6 @@
 function setup_my_go_path {
   # Depend on goenv; see also `.zsh/goenv.zsh`
-  local _path="$( find ~/go -maxdepth 1 -mindepth 1 -type d | sort | tail -n1 )"
+  local _path="$( find ~/go -maxdepth 1 -mindepth 1 -type d | sort --version-sort | tail -n1 )"
 
   if [ "$_path" ]; then
     export PATH="$PATH:$_path/bin"
