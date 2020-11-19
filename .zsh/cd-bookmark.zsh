ed51e1a1604fa538dd0451ca96983304019b058e
kg8m <takumi.kagiyama@gmail.com>
2020-11-19 20:36:56 +0900

    Execute `zinit creinstall` also on install

--- .zsh/cd-bookmark.zsh
+++ .zsh/cd-bookmark.zsh
@@ -3,5 +3,5 @@ function setup_my_cdbookmark {
   alias cdb="cd-bookmark"
   unset -f setup_my_cdbookmark
 }
-zinit ice lucid wait"0c" blockf atpull"zinit creinstall -q \$(pwd)" atload"setup_my_cdbookmark"
+zinit ice lucid wait"0c" blockf atclone"zinit creinstall -q \$(pwd)" atpull"%atclone" atload"setup_my_cdbookmark"
 zinit light mollifier/cd-bookmark
