89bdc67a39ad6e6a23e6a60c97cb35aa92fef97e
kg8m <takumi.kagiyama@gmail.com>
2020-11-19 20:48:11 +0900

    Shorten the `$PATH`

--- .zsh/rubocop.zsh
+++ .zsh/rubocop.zsh
@@ -1,2 +1,10 @@
-zinit ice lucid wait"0c" as"program" pick"bin/rubocop-daemon-wrapper"
+# Don't use zinit's options like `as"command" pick"bin/rubocop-daemon-wrapper"` because it makes the `$PATH` longer and
+# longer. Make symbolic links in `$HOME/bin` instead.
+function setup_my_rubocop_daemon {
+  local binary="bin/rubocop-daemon-wrapper"
+
+  mkdir -p "$HOME/bin"
+  ln -fs "$(pwd)/${binary}" "$HOME/bin/$(basename "$binary")"
+}
+zinit ice lucid wait"0c" as"null" atclone"setup_my_rubocop_daemon" atpull"%atclone"
 zinit light fohte/rubocop-daemon
