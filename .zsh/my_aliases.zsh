1cb1903cc680521ecb3cc2f4bb30c407f657eed3
kg8m <takumi.kagiyama@gmail.com>
2020-12-20 08:51:38 +0900

    Don't inherit current `$TERM` to remote servers

--- .zsh/my_aliases.zsh
+++ .zsh/my_aliases.zsh
@@ -13,5 +13,10 @@ alias diff="diff -U 10 -b -B"
 alias ls="ls --color -a"
 alias ll="ls -l --time-style=long-iso"
 alias rm="rm -i"
+
+# Don't inherit `$TERM` to remote servers.
+# `xterm-256color-italic` is set in tmux.
+alias ssh="TERM=xterm-256color ssh"
+
 alias sudo='sudo env PATH=$PATH'
 alias watch="watch --color"
