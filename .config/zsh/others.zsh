1087fae0ff41ab2ab6016359120afd65882ffba7
kg8m <takumi.kagiyama@gmail.com>
2021-05-15 12:11:55 +0900

    zsh: open the directory to install tbkeys after update

--- .config/zsh/others.zsh
+++ .config/zsh/others.zsh
@@ -1,9 +1,6 @@
 function plugin:setup:others {
   if [ "$(uname)" = "Darwin" ]; then
-    function plugin:setup:tbkeys {
-      execute_with_echo "cp tbkeys.xpi $HOME/Downloads"
-    }
-    zinit ice lucid as"null" from"gh-r" bpick"tbkeys.xpi" atclone"plugin:setup:tbkeys" atpull"%atclone"
+    zinit ice lucid as"null" from"gh-r" bpick"tbkeys.xpi" atclone"execute_with_echo 'open .'" atpull"%atclone"
     zinit light wshanks/tbkeys
   fi
 
