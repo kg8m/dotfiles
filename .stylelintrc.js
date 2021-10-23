f60d7000adef56186814f6e569da8c1a5466acd6
kg8m <takumi.kagiyama@gmail.com>
2021-10-23 14:54:17 +0900

    stylelint: udpate rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,17 @@ const modulesPath = path.join(dataHomePath, "yarn/global/node_modules");
 module.exports = {
   extends: [
     path.join(modulesPath, "stylelint-config-standard"),
-  ]
-}
+  ],
+  rules: {
+    "color-hex-length": "long",
+    "comment-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "no-descending-specificity": null,
+
+    // kebab-case or snake_case
+    "selector-class-pattern": "^([a-z][a-z0-9]*)([-_][a-z0-9]+)*$",
+    "selector-id-pattern": "^([a-z][a-z0-9]*)([-_][a-z0-9]+)*$",
+
+    "selector-list-comma-newline-after": null,
+  },
+};
