f9e2f2197e14abd3db8c33be9e6d9692d24b3281
kg8m <takumi.kagiyama@gmail.com>
2021-11-20 20:08:15 +0900

    zsh: show hidden files/directories but hide `.` and `..` as result of `ls` command

--- .config/zsh/my-aliases.zsh
+++ .config/zsh/my-aliases.zsh
@@ -10,7 +10,7 @@ if crontab -i > /dev/null 2>&1; then
 fi
 
 alias diff="diff -U 10 -b -B"
-alias ls="ls --color -a -v"
+alias ls="ls --color --almost-all -v"
 alias ll="ls -l --time-style=long-iso"
 alias rm="rm -i"
 
