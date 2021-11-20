bb7a37b0d09e1bb876d70c80079ab8d3d3798572
kg8m <takumi.kagiyama@gmail.com>
2021-11-20 20:07:37 +0900

    zsh: overwrite dircolors

--- .config/zsh/colors.zsh
+++ .config/zsh/colors.zsh
@@ -1,16 +1,39 @@
 function plugin:setup:dircolors {
   if [ ! -f "${KG8M_ZSH_CACHE_DIR:?}/dircolors_ansi-universal" ]; then
+    local tempfile="$(mktemp)"
+
+    cat dircolors.ansi-universal > "${tempfile}"
+
+    # shellcheck disable=SC2034
+    local overwrites=(
+      # bright black (gray)
+      ".DS_Store 01;30"
+      ".example 01;30"
+
+      # green: editable text
+      ".diff 32"
+      ".json 32"
+      ".toml 32"
+      ".ts 32"
+      ".vimrc 32"
+      ".vue 32"
+      ".yaml 32"
+      ".yml 32"
+      ".zshenv 32"
+      ".zshrc 32"
+    )
+
+    echo "${(j:\n:)overwrites}" >> "${tempfile}"
+
     if command -v gdircolors > /dev/null; then
       local dircolors=gdircolors
     else
       local dircolors=dircolors
     fi
 
-    # My original colors:
-    #   export LSCOLORS=gxfxxxxxcxxxxxxxxxgxgx
-    #   export LS_COLORS='di=01;36:ln=01;35:ex=01;32'
-    #   zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'ex=32'
-    $dircolors dircolors.ansi-universal > "${KG8M_ZSH_CACHE_DIR}/dircolors_ansi-universal"
+    "${dircolors}" "${tempfile}" > "${KG8M_ZSH_CACHE_DIR}/dircolors_ansi-universal"
+    rm -f "${tempfile}"
+
     zcompile "${KG8M_ZSH_CACHE_DIR}/dircolors_ansi-universal"
   fi
   source "${KG8M_ZSH_CACHE_DIR}/dircolors_ansi-universal"
@@ -18,5 +41,11 @@ function plugin:setup:dircolors {
 
   unset -f plugin:setup:dircolors
 }
-zinit ice lucid wait"0c" atload"plugin:setup:dircolors"
+
+function plugin:setup:dircolors:reset {
+  echo -e "\n$(highlight:yellow "Reset colors, so check updates and fix my overwrites if needed.")\n"
+  trash "${KG8M_ZSH_CACHE_DIR:?}/dircolors_ansi-universal"
+}
+
+zinit ice lucid wait"0c" atclone="plugin:setup:dircolors:reset" atpull="%atclone" atload"plugin:setup:dircolors"
 zinit light seebi/dircolors-solarized
