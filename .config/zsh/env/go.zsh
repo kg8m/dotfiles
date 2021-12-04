8d7e98666b52bc62a1bbce6dfd3b451eb3b40fa2
kg8m <takumi.kagiyama@gmail.com>
2021-12-04 17:26:13 +0900

    zsh: cache gopath

--- .config/zsh/env/go.zsh
+++ .config/zsh/env/go.zsh
@@ -1,6 +1,9 @@
 function() {
-  # Depend on goenv; see also `.config/zsh/goenv.zsh`
-  local gopath="$(find ~/go -maxdepth 1 -mindepth 1 -type d | sort --version-sort | tail -n1)"
+  if [ ! -f "${KG8M_ZSH_CACHE_DIR:?}/gopath" ]; then
+    # Depend on goenv; see also `.config/zsh/goenv.zsh`
+    find ~/go -maxdepth 1 -mindepth 1 -type d | sort --version-sort | tail -n1 > "${KG8M_ZSH_CACHE_DIR}/gopath"
+  fi
+  local gopath="$(cat "${KG8M_ZSH_CACHE_DIR}/gopath")"
 
   if [ -n "${gopath}" ] && [ -d "${gopath}/bin" ]; then
     path+=("${gopath}/bin")
