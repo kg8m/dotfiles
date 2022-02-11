610a3db6e454d8e84bbad795eb04a2dd9ec2341c
kg8m <takumi.kagiyama@gmail.com>
2022-02-11 15:16:21 +0900

    ERB: disable syntax check if on a Rails project

---
+++ .config/efm-langserver/erb_wrapper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if ! command -v erb > /dev/null; then
+  exit 1
+fi
+
+# Don't check if on Rails project because some valid syntaxes extended by Erubi are detected as invalid by `erb` command.
+if [ -d "app" ] && [ -f "config/environment.rb" ]; then
+  exit 0
+fi
+
+# Always show errors on the first line because converted Ruby code doesn't match the original ERB code.
+erb -x -T - -P | ruby -c 2>&1 1> /dev/null | sed -e 's/^-:/1:/'
