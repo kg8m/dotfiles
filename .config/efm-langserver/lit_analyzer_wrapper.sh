47fd4d6fe7b8e13e4e09101d6325d0fb1ba1b68a
kg8m <takumi.kagiyama@gmail.com>
2022-05-16 01:24:55 +0900

    Shell/Node.js: execute lit-analyzer only when it is allowed

---
+++ .config/efm-langserver/lit_analyzer_wrapper.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+if ! command -v lit-analyzer > /dev/null; then
+  exit 1
+fi
+
+if [ ! "${LIT_ANALYZER_AVAILABLE:-}" = "1" ]; then
+  exit 1
+fi
+
+# Always use system Node.js version for lit-analyzer.
+export ASDF_NODEJS_VERSION="system"
+
+filepath="${1:?}"
+args=(
+  "${filepath}"
+  --format list
+  --noColor
+  --strict
+  "${=LIT_ANALYZER_EXTRA_OPTIONS}"
+)
+
+lit-analyzer "${args[@]}"
