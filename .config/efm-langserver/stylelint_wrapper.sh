c8a75ef33b78db33bc046034f92b6e7049dda810
kg8m <takumi.kagiyama@gmail.com>
2022-08-13 22:48:20 +0900

    Shell: give a filepath to the stylelint wrapper

--- .config/efm-langserver/stylelint_wrapper.sh
+++ .config/efm-langserver/stylelint_wrapper.sh
@@ -3,6 +3,8 @@ if ! command -v stylelint > /dev/null; then
   exit 1
 fi
 
+target_filepath="${1:?}"
+
 node_version="$(asdf current nodejs | awk '{ print $2 }')"
 config_basedir="${ASDF_DATA_DIR:?}/installs/nodejs/${node_version}/.npm/lib"
 
@@ -12,9 +14,11 @@ if [ ! -d "${config_basedir}" ]; then
 fi
 
 options=(
+  # For `extends` in `.stylelintrc.js`
   --config-basedir "${config_basedir}"
+
   --stdin
-  --stdin-filename /dummy.css
+  --stdin-filename "${target_filepath}"
   --formatter unix
   --no-color
 )
