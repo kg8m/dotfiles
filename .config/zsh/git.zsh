134e35ee72583da4f6d933671a6ed9df6e0d5081
kg8m <takumi.kagiyama@gmail.com>
2023-03-18 01:56:42 +0900

    Git: treat the `origin` branch as a protected local branch for newer Git

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -1,2 +1,2 @@
-export GIT_PROTECTED_LOCAL_BRANCH_PATTERN='^(master|main) '
+export GIT_PROTECTED_LOCAL_BRANCH_PATTERN='^(master|main|origin) '
 export GIT_PROTECTED_REMOTE_BRANCH_PATTERN='^(master|main) '
