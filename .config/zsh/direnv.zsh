666f67efb27852673a99ff8e00cc0381c8e1c855
kg8m <takumi.kagiyama@gmail.com>
2023-10-08 02:31:54 +0900

    zsh: fix a direnv reference URL

--- .config/zsh/direnv.zsh
+++ .config/zsh/direnv.zsh
@@ -9,7 +9,7 @@ function plugin:direnv:atload {
     source "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh"
 
     # Don't cache the result of `direnv export zsh` because it varies depending on each directory's `.envrc`
-    # https://github.com/direnv/direnv/blob/a4632773637ee1a6b08fa81043cacd24ea941489/shell_zsh.go#L12
+    # https://github.com/direnv/direnv/blob/93cd37820c29a1f96a0e64ff1557a7a912866d4f/internal/cmd/shell_zsh.go#L12
     eval "$(direnv export zsh)"
   else
     # shellcheck disable=SC2317
