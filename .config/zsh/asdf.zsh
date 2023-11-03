cca6efebe02df7e3f8ccc4e77a80fb83b3894be1
kg8m <takumi.kagiyama@gmail.com>
2023-11-03 13:29:13 +0900

    zsh: add a utility function to check newer version of asdf plugins

--- .config/zsh/asdf.zsh
+++ .config/zsh/asdf.zsh
@@ -34,3 +34,34 @@ function plugin:asdf:atclone {
 
 zinit ice lucid wait"0c" as"null" atclone"plugin:asdf:atclone"
 zinit light asdf-vm/asdf
+
+function asdf:plugins:upgrade:check {
+  # shellcheck disable=SC2317
+  function asdf:plugins:upgrade:check:do {
+    local plugin="${1:?}"
+    shift 1
+
+    local local_latest="$(asdf list "${plugin}" "$@" | tail -n1 | sd '^\s*\*?' '')"
+    local remote_latest="$(asdf latest "${plugin}" "$@")"
+
+    plugin="$(printf '%-10s' "${plugin}")"
+    local_latest="$(printf '%-10s' "${local_latest}")"
+    remote_latest="$(printf '%-10s' "${remote_latest}")"
+
+    if [ ! "${local_latest}" = "${remote_latest}" ]; then
+      remote_latest="$(highlight:yellow "${remote_latest}")"
+    fi
+
+    echo "${plugin} local ${local_latest} remote ${remote_latest}"
+  }
+
+  local plugins=("${(@f)$(asdf plugin list)}")
+  local plugin
+  for plugin in "${plugins[@]}"; do
+    asdf:plugins:upgrade:check:do "${plugin}"
+
+    if [ "${plugin}" = "python" ]; then
+      asdf:plugins:upgrade:check:do "${plugin}" "2"
+    fi
+  done
+}
