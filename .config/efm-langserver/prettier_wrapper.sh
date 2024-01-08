a73154a97c5c4c6680c30da1271973d6f00c2e14
kg8m <takumi.kagiyama@gmail.com>
2024-01-08 15:36:54 +0900

    Shell: disable Prettier in case `should_use_deno` is true only for TypeScript files

--- .config/efm-langserver/prettier_wrapper.sh
+++ .config/efm-langserver/prettier_wrapper.sh
@@ -3,22 +3,25 @@ if ! command -v prettier > /dev/null; then
   exit 1
 fi
 
-if [ "$(should_use_deno)" = "1" ]; then
-  exit 1
-fi
-
 target_filepath="${1:?}"
 
-if command -v eslint > /dev/null; then
-  case "${target_filepath}" in
-    *.js | *.jsx | *.ts | *.tsx | *.vue)
+case "${target_filepath}" in
+  *.js | *.jsx | *.ts | *.tsx | *.vue)
+    if command -v eslint > /dev/null; then
       # Use ESLint as formatter.
       if [ "${ESLINT_AS_FORMATTER:-}" = "1" ] && [ ! "${ESLINT_AND_PRETTIER_AS_FORMATTER:-}" = "1" ]; then
         exit 1
       fi
-      ;;
-  esac
-fi
+    else
+      if [[ "${target_filepath}" =~ \.ts$ ]]; then
+        # Use Deno as formatter.
+        if [ "$(should_use_deno)" = "1" ]; then
+          exit 1
+        fi
+      fi
+    fi
+    ;;
+esac
 
 options=(
   --stdin-filepath "$("${XDG_CONFIG_HOME:?}/efm-langserver/format_filepath" "${target_filepath}")"
