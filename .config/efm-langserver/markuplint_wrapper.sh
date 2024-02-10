2356723dfdd25e2376e9224974ee640e214ac05c
kg8m <takumi.kagiyama@gmail.com>
2024-02-10 17:20:39 +0900

    Shell: don’t use Markuplint for non-HTML ERB files

--- .config/efm-langserver/markuplint_wrapper.sh
+++ .config/efm-langserver/markuplint_wrapper.sh
@@ -12,6 +12,16 @@ export ASDF_NODEJS_VERSION="$(newest_version nodejs)"
 
 target_filepath="${1:?}"
 
+case "${target_filepath}" in
+  *.html.erb)
+    # It is a target file; do nothig.
+    ;;
+  *.erb)
+    # Non-HTML ERB files are not supported.
+    exit 0
+    ;;
+esac
+
 # BSD `realpath` doesn't support `--relative-to`.
 if command -v grealpath > /dev/null; then
   realpath_bin="grealpath"
