6d4153ef8cbf1d9318d7042462e3706228a610df
kg8m <takumi.kagiyama@gmail.com>
2024-05-22 00:47:00 +0900

    denops: improve type-safety by using `Entrypoint` introduced at v6.5.0

--- .config/vim/denops/kg8m/main.ts
+++ .config/vim/denops/kg8m/main.ts
@@ -1,8 +1,8 @@
-import type { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
+import type { Entrypoint } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
 import { show } from "./dein/update/logs.ts";
 
-export function main(denops: Denops): void {
+export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
     "dein:update:logs:show": async () => await show(denops),
   };
-}
+};
