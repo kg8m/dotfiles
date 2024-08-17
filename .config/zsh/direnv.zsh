892cc9c12de4a10435259e66b70161c6e8f6008c
kg8m <takumi.kagiyama@gmail.com>
2024-08-17 13:52:39 +0900

    Zsh: remove unnecessary lazy loading configs for direnv

--- .config/zsh/direnv.zsh
+++ .config/zsh/direnv.zsh
@@ -1,37 +1,21 @@
-function plugin:direnv:atload {
-  if ((${+commands[direnv]})); then
-    if [ ! -f "${XDG_CACHE_HOME:?}/zsh/direnv_hook.zsh" ]; then
-      direnv hook zsh > "$(ensure_dir "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh")"
+if ((${+commands[direnv]})); then
+  if [ ! -f "${XDG_CACHE_HOME:?}/zsh/direnv_hook.zsh" ]; then
+    direnv hook zsh > "$(ensure_dir "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh")"
 
-      # cf. zsh:rcs:compile() and zsh:rcs:compile:clear()
-      zcompile "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh"
+    # cf. zsh:rcs:compile() and zsh:rcs:compile:clear()
+    zcompile "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh"
+  fi
+  source "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh"
+
+  # Don't cache the result of `direnv export zsh` because it varies depending on each directory's `.envrc`
+  # https://github.com/direnv/direnv/blob/93cd37820c29a1f96a0e64ff1557a7a912866d4f/internal/cmd/shell_zsh.go#L12
+  eval "$(direnv export zsh)"
+else
+  # shellcheck disable=SC2317
+  function plugin:direnv:not_installed_error {
+    if [ -f .envrc ]; then
+      echo:warn ".envrc exists but direnv isn't installed."
     fi
-    source "${XDG_CACHE_HOME}/zsh/direnv_hook.zsh"
-
-    # Don't cache the result of `direnv export zsh` because it varies depending on each directory's `.envrc`
-    # https://github.com/direnv/direnv/blob/93cd37820c29a1f96a0e64ff1557a7a912866d4f/internal/cmd/shell_zsh.go#L12
-    eval "$(direnv export zsh)"
-  else
-    # shellcheck disable=SC2317
-    function plugin:direnv:not_installed_error {
-      if [ -f .envrc ]; then
-        echo:warn ".envrc exists but direnv isn't installed."
-      fi
-    }
-    add-zsh-hook chpwd plugin:direnv:not_installed_error
-  fi
-
-  unset -f plugin:direnv:atload
-}
-
-function() {
-  local ice_options=(lucid nocd atload"plugin:direnv:atload")
-
-  # immediately load direnv and, reflect envs if `.envrc` exists.
-  if [ ! -f ".envrc" ]; then
-    ice_options+=(wait"0b")
-  fi
-
-  zinit ice "${ice_options[@]}"
-  zinit snippet "${XDG_CONFIG_HOME:?}/zsh/null/plugin-direnv-atload"
-}
+  }
+  add-zsh-hook chpwd plugin:direnv:not_installed_error
+fi
